9ca558d2e04e18029b3fdb1bc4d3acc742912ff5
alexmuniz96 <alexmuniz096@gmail.com>
2022-05-25 22:35:38 -0300

    first-commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <p>Pokedex</p>
+      <form>
+        <input placeholder="Busque pelo nome - icon" />
+      </form>
+    </header>
+  )
+}
