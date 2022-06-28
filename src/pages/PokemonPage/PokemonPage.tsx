3ccfb66d87f70669c375b60c357956faecc37847
alexmuniz96 <alexmuniz096@gmail.com>
2022-06-27 22:13:35 -0300

    feat: service list pokemon

--- src/pages/PokemonPage/PokemonPage.tsx
+++ src/pages/PokemonPage/PokemonPage.tsx
@@ -1,18 +1,39 @@
+import { useEffect, useState } from 'react'
 import styles from './styles.module.scss'
 import PokemonCard from '../../components/PokemonCard'
+import api from '../../services/api'
+import axios from 'axios';
+import { listPokemon } from '../../services/ListPokemon';
+
+interface PokemonProps {
+  name: string,
+  url: string,
+}
 
 export default function PokemonPage() {
+  const [pokemonData, setPokemonData] = useState<PokemonProps[]>([]);
 
-  // useEffect(() => {
-  //   api.get()
-  // }, [])
+  useEffect(() => {
 
+    api.get('/pokemon')
+      .then((response) => (
+        setPokemonData(response.data.results)
+      ))
+
+  }, [])
+  listPokemon()
   return (
     <main className={styles.pokemonsContainer}>
       <section>
+        {pokemonData.map(pokemon => (
+          <p key={pokemon.name}> {pokemon.name}</p>
+        ))}
+        {/* <PokemonCard />
         <PokemonCard />
         <PokemonCard />
         <PokemonCard />
+        <PokemonCard />
+        <PokemonCard /> */}
       </section>
     </main>
   )
---
+++ src/services/ListPokemon.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiBaseUrl from './api';
+
+export interface PokemonListInterface {
+  name: string,
+  url: string
+}
+
+interface ListPokemonsInterface {
+  count: number;
+  next: null | string;
+  previous: null | string;
+  results: PokemonListInterface[];
+}
+
+export async function listPokemon(): Promise<ListPokemonsInterface> {
+  const response = await apiBaseUrl.get<ListPokemonsInterface>('pokemon');
+  console.log(response.data)
+  return response.data
+}
+
